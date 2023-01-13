75b4e4741ec79b202f18ea743f22006136500c42
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-13 22:05:36 +0500

    Import http, define port, create server, start server

---
+++ server.js
@@ -0,0 +1,11 @@
+// Importing http from node.js
+const http = require('http');
+
+// Port at which the project will run   // Getting it from env var or hard code it
+const port = process.env.PORT || 3000;
+
+// Creating a server // there is a parameter called listener that works whenever there is a new request
+const server = http.createServer();
+
+// To start the server and start listening on the port  // Then it will execute the listener in createServer()
+server.listen(port);
