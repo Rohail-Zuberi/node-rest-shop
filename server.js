5f0060fe39d3a1b6d226b51a0caff6752a3eed1d
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-13 22:20:04 +0500

    Import app into server.js and pass it as a listener to createServer()

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // Importing http from node.js
 const http = require('http');
+const app = require('./app');
 
 // Port at which the project will run   // Getting it from env var or hard code it
 const port = process.env.PORT || 3000;
 
 // Creating a server // there is a parameter called listener that works whenever there is a new request
-const server = http.createServer();
+const server = http.createServer(app);
 
 // To start the server and start listening on the port  // Then it will execute the listener in createServer()
 server.listen(port);
