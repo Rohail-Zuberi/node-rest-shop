41434356bed9c340b784f7337efd3556f30437e7
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 13:50:11 +0500

    Create order.js and add its routes

---
+++ api/routes/orders.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// router is an express sub package to handle different routes reaching different end points with different http verbs
+const router = express.Router();
+
+
+
+module.exports = router;
