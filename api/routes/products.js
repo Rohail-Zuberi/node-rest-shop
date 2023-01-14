44d8cd7116747167f8cbd76608c968b474279382
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 13:38:48 +0500

    Handling the requests
    - GET request handled according to project
    - POST request handled according to project
    - GET request for specific id tested by param 'productID'

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,4 +3,33 @@ const express = require('express');
 // router is an express sub package to handle different routes reaching different end points with different http verbs
 const router = express.Router();
 
-router.get('/');
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST requests to /products'
+    });
+});
+
+// '/:productID' productID is a variable 
+router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID; // Extracting the product ID
+
+    // To check is the param is being read properly
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'you discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'you passed an ID' 
+        });
+    }
+});
+
+module.exports = router;
