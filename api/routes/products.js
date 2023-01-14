4812684b6d772a1e6ea91932577b6e65746f7622
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 13:13:24 +0500

    Create api folder for apis

---
+++ api/routes/products.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+// router is an express sub package to handle different routes reaching different end points with different http verbs
+const router = express.Router();
+
+router.get('/');
