3cd0d5aceec4e8939a3c55e0cd386a2e0ca6efa9
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 14:02:38 +0500

    Add orders route to pass requests to

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
 // .use method of express means that every request will have to go through app.use(...)
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
