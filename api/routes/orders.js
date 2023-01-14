2da5143f5a723476e60146281224828d86d5864e
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 14:03:23 +0500

    Create routes for order.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,6 +3,31 @@ const express = require('express');
 // router is an express sub package to handle different routes reaching different end points with different http verbs
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
 
+// Status code 201 or ending with 1 is more appropriate for, everything was successful
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    });
+});
+
+router.get('/:orderID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderID
+    });
+});
+
+router.delete('/:orderID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted',
+        orderId: req.params.orderID
+    });
+});
 
 module.exports = router;
