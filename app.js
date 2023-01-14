3fa80b08f1ada7b8d11fcbbafdd5c14462d1d56e
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 14:33:23 +0500

    Install morgan to log reqeusts and use it in app.js

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 const express = require("express");
 const app = express();
 
+// logging package for node.js
+// Tell express to funnel all requests through this middleware
+// Morgan will log the request and let it continue
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // .use method of express means that every request will have to go through app.use(...)
 app.use('/products', productRoutes);
