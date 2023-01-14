d14846c25ba5e2378ee6081bdf26fee1c661c744
Mohammad Rohail Zuberi <rohailzuberi5@gmail.com>
2023-01-14 13:38:04 +0500

    route all requests with /product end point to productRoutes

--- app.js
+++ app.js
@@ -2,11 +2,9 @@
 const express = require("express");
 const app = express();
 
+const productRoutes = require('./api/routes/products');
+
 // .use method of express means that every request will have to go through app.use(...)
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "it works!",
-  });
-});
+app.use('/products', productRoutes);
 
 module.exports = app;
